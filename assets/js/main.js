2dc8f4e27013f9515bd1acf02e6d150ab0410a96
liamsegura <liamdiegosegura@gmail.com>
2022-04-20 21:48:49 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+//event listener to shuffle an array containing 1 bullet, and 5 blanks
+
+//event listener that iterates through the chamber index
+
+//function that shuffles an array completely randomly once and stores this into a variable 
+
+//function that iterates through the chambers index and checks weather the current index contains the bullet
