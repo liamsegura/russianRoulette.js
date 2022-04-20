0981cc4da1c4dbab8bd60cb2f0836701099c8828
liamsegura <liamdiegosegura@gmail.com>
2022-04-20 22:08:38 +0100

    added an event listener for the chamber, and created a random shuffle function for the chambers array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
 //event listener to shuffle an array containing 1 bullet, and 5 blanks
+document.getElementById('chamber').addEventListener('click', shuffle)
 
 //event listener that iterates through the chamber index
 
 //function that shuffles an array completely randomly once and stores this into a variable 
+function shuffle(){
+    let chamber = ['BANG!', 'Click...', 'Click...', 'Click...', 'Click...', 'Click...']
+    let randomSpin = chamber
+        //place each element in the array in an object, and give it a random sort key
+         .map(value => ({ value, sort: Math.random() }))
+        //sort using random key
+         .sort((a, b) => a.sort - b.sort)
+        //unmap to get the original objects 
+         .map(({ value }) => value)
+  console.log(randomSpin)
+}
 
 //function that iterates through the chambers index and checks weather the current index contains the bullet
