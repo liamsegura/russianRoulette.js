358c0d5a06b319f8eeccdbc29cb11a510766f728
liamsegura <liamdiegosegura@gmail.com>
2022-04-20 23:42:35 +0100

    added functionality to the  trigger, still needs work. on load the works fine, still need to store randomized array on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 //event listener to shuffle an array containing 1 bullet, and 5 blanks
-document.getElementById('chamber').addEventListener('click', shuffle)
+document.getElementById('chamber').addEventListener('click', chamberSpin)
 
 //event listener that iterates through the chamber index
-
+document.getElementById('trigger').addEventListener('click', trigger)
 //function that shuffles an array completely randomly once and stores this into a variable 
-function shuffle(){
+function chamberSpin(){
     let chamber = ['BANG!', 'Click...', 'Click...', 'Click...', 'Click...', 'Click...']
     let randomSpin = chamber
         //place each element in the array in an object, and give it a random sort key
@@ -13,7 +13,28 @@ function shuffle(){
          .sort((a, b) => a.sort - b.sort)
         //unmap to get the original objects 
          .map(({ value }) => value)
-  console.log(randomSpin)
+         console.log('chamber spun')
+         console.log(randomSpin)
+  return randomSpin
 }
 
+//global variable for the index which increases if conditions aren't met
+let index = 0
+
+let chambered = chamberSpin()
+
+
 //function that iterates through the chambers index and checks weather the current index contains the bullet
+function trigger(){
+    let chamber = chambered
+if(chamber[index] == 'Click...'){
+    console.log('ooof')
+    console.log(chamber)
+    return index += 1
+}
+else{
+    console.log('dead')
+    console.log(chamber)
+    return index = 0
+}
+}
